2e55e051b7d4a9c35f7843c9d9fc3cae2a08d219
Linus Torvalds <136006237+worian4@users.noreply.github.com>
2025-08-06 14:44:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from core.ascii_video_lib import ascii_video
 
 vid = ascii_video()
 
-input_path = 'videos/11.mp4'
-video_path = 'video'
+input_path = 'yourvideo.mp4'
+video_path = 'videodir'
 
 vid.ascii_convert(input_path, video_path, 'txt', new_width=500, rgb=1)
 
 vid.read_video(video_path)
+
